dbeea84718b626fe72659d7fc82065fb497966a4
Rebecca Tong <rtong@buffalo.edu>
2018-08-15 20:28:59 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -32,7 +32,7 @@ class FiguresController < ApplicationController
   end
 
   get '/figures/:id/edit' do
-    @figure = Figure.find_by(id: params[:id])
+    @figure = Figure.find_by_id(params[:id])
     @titles = Title.all
     erb :'/figures/edit'
   end
